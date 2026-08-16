0ad31b091f9d22fcbfc1b1943ecbde2e74a8cdd9
TopUni uzbekistan <tillo9841@gmail.com>
2026-08-16 12:24:43 +0500

    refactor: switch public site to multi-page institutional app

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import SiteApp from './site/SiteApp'
 import AdminApp from './admin/AdminApp'
 import './styles.css'
 import './admin/admin.css'
@@ -8,5 +8,5 @@ import './admin/admin.css'
 const isAdmin = window.location.pathname.startsWith('/admin')
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>{isAdmin ? <AdminApp /> : <App />}</StrictMode>,
+  <StrictMode>{isAdmin ? <AdminApp /> : <SiteApp />}</StrictMode>,
 )
