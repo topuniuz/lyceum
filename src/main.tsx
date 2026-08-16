d5d1d0b20e819fa5d4774b3a1fe563d9eb06594c
TopUni uzbekistan <tillo9841@gmail.com>
2026-08-16 13:23:45 +0500

    fix: load final site contrast safeguards after base styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import SiteApp from './site/SiteApp'
 import AdminApp from './admin/AdminApp'
 import './styles.css'
+import './site/contrast-fixes.css'
 import './admin/admin.css'
 
 const isAdmin = window.location.pathname.startsWith('/admin')
